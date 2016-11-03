00b4d804886d9037ac420453faebba06d0a11eb9
nalabjp <nalabjp@gmail.com>
2016-11-04 00:23:08 +0900

    :recycle: Refactor

--- lib/active_prepender.rb
+++ lib/active_prepender.rb
@@ -1,19 +1,23 @@
 require "active_prepender/version"
 
 module ActivePrepender
-  def prepend_features(base)
-    super
+  module Prependable
+    def prepend_features(base)
+      super
 
-    included_modules.reverse.push(self).each do |mod|
-      base.singleton_class.prepend(mod.const_get(:ClassMethods)) if mod.const_defined?(:ClassMethods)
+      included_modules.reverse.push(self).each do |mod|
+        base.singleton_class.prepend(mod.const_get(:ClassMethods)) if mod.const_defined?(:ClassMethods)
+      end
     end
   end
+
+  include Prependable
 
   module WithInclude
     def append_features(base)
       return false if super == false
 
-      base.extend(ActivePrepender)
+      base.extend(Prependable)
     end
   end
 end
